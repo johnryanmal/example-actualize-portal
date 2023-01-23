bf79ba88a65daa1640cc10a138c0d1b461fe3475
johnryanmal <johnryanmal@gmail.com>
2023-01-23 09:52:55 -0800

    add pagination to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where(role_id: 1)
+    page_size = params[:page_size]&.to_i || 10
+    page = params[:page]&.to_i || 0
+    offset = page_size * page
+
+    @users = User.where(role_id: 1).limit(page_size).offset(offset)
     render template: "users/index"
   end
 end
